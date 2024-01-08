a405790a9a17bd009083c7dad6be81595b02d2c2
Diego Cusmai <112570306+dcusmai@users.noreply.github.com>
2024-01-08 10:47:59 -0300

    First Commit: initializing project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express(); // Este puede ser app o server por convención
+
+
+
+app.listen(3001, () => {
+    console.log('Servidor listo en el 3001, capo!')
+})
